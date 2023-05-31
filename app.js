e24c29b8de19e84a4c5995d18043a0324d0f28b3
disaacjm <disaacjm@gmail.com>
2023-05-31 13:24:17 +0100

    handlebars (views, layout, partials)

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
-
+const hbs = require("hbs")
 const app = express();
 
 app.use(express.static("public")); // Make everything inside of public/ available
+
+app.set("views", __dirname + "/views"); // tells our Express app where to look for our views
+
+app.set("view engine", "hbs"); // sets HBS as the template engine
+
+hbs.registerPartials(__dirname + "/views/partials"); //tell HBS which directory we use for partials
+
 
 // app.get(path, code);
 // app.get(path, (req, res, next) => {});
@@ -11,12 +18,46 @@ app.use(express.static("public")); // Make everything inside of public/ availabl
 app.get("/", (req, res, next) => {
   console.log("we have received a request for the HOMEPAGE");
 
-  res.sendFile(__dirname + "/views/home-page.html");
+  res.render("home-page")
 });
 
 // GET /contact
 app.get("/contact", (req, res, next) => {
-  res.sendFile(__dirname + "/views/contact-page.html");
+  res.render("contact-page")
+});
+
+app.get("/pizzas/margarita", (req, res, send) => {
+  // res.send("page for margarita");
+
+  const pizzaDetails = {
+    title: "Pizza Margarita",
+    price: 12,
+    imageFile: "pizza_margarita.jpg",
+    ingredients: ['mozzarella', 'tomato sauce', 'basilicum'],
+  };
+  res.render("product", pizzaDetails);
+});
+
+app.get("/pizzas/veggie", (req, res, send) => {
+  // res.send("page for veggie")
+  const pizzaDetails = {
+    title: "Pizza Veggie",
+    price: 14,
+    imageFile: "pizza_veggie.jpg",
+    ingredients: ['cherry tomatoes', 'basilicum', 'Olives'],
+  };
+  res.render("product", pizzaDetails);
+});
+
+app.get("/pizzas/seafood", (req, res, send) => {
+  // res.send("page for seafood")
+  const pizzaDetails = {
+    title: "Pizza Seafood",
+    imageFile: "pizza_seafood.jpg",
+    ingredients: ['tomato sauce', 'garlic', 'prawn'],
+  };
+
+  res.render("product", pizzaDetails);
 });
 
 app.listen(3000, () => {
